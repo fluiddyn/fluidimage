bd8b9e80b3e1cb43413d0fbfa4151595400ed6c4
Cyrille Bonamy <cyrille.bonamy@legi.cnrs.fr>
2016-03-02 00:15:25 +0100

    dev correl_pythran (mode=same)

--- fluidimage/calcul/correl_pythran.py
+++ fluidimage/calcul/correl_pythran.py
@@ -1,7 +1,56 @@
-
+import numpy as np
 
 # pythran export correl_pythran(float32[:,:], float32[:,:], float)
-def correl_pythran(im0, im1, displacement_max):
 
-    raise NotImplementedError
-    return 0.
+
+def correl_pythran(im0, im1, disp_max):
+    """Correlations by hand using only numpy.
+
+    disp_max => filter image size (im1_crop)
+
+     .. math::
+     """
+    # correl (\xi_x, \xi_y) = \int_{im0 inter im1} im0*im1_{\delta \xi}
+
+    ny = disp_max * 2 + 1
+    nx = ny
+    ny0, nx0 = im0.shape
+    ny1, nx1 = im1.shape
+
+#  if mode == 'same':
+
+    correl = np.zeros((ny, nx), dtype=np.float32)
+
+    for xix in range(nx // 2 + 1):
+        dispx = -disp_max + 2 * disp_max * xix // (nx - 1)
+        for xiy in range(ny // 2 + 1):
+            dispy = -disp_max + 2 * disp_max * xiy // (ny - 1)
+            for ix in range(nx0 + dispx):
+                for iy in range(ny0 + dispy):
+                    correl[xiy, xix] += im0[iy - dispy,
+                                            ix - dispx] * im1[iy, ix]
+        for xiy in range(ny // 2):
+            dispy = disp_max * (xiy + 1) // (ny - 1)
+            for ix in range(nx0 + dispx):
+                for iy in range(ny0 - dispy):
+                    correl[xiy + disp_max + 1,
+                           xix] += im0[iy, ix - dispx] * im1[iy + dispy, ix]
+    for xix in range(nx // 2):
+        dispx = disp_max * (xix + 1) // (nx - 1)
+        for xiy in range(ny // 2 + 1):
+            dispy = -disp_max + 2 * disp_max * xiy // (ny - 1)
+            for ix in range(nx0 - dispx):
+                for iy in range(ny0 + dispy):
+                    correl[xiy,
+                           xix + disp_max + 1] += im0[iy - dispy,
+                                                      ix] * im1[iy, ix + dispx]
+        for xiy in range(ny // 2):
+            dispy = disp_max * (xiy + 1) // (ny - 1)
+            for ix in range(nx0 - dispx):
+                for iy in range(ny0 - dispy):
+                    correl[xiy + disp_max + 1,
+                           xix + disp_max + 1] += im0[iy, ix] * im1[iy + dispy,
+                                                                    ix + dispx]
+# else TODO mode='valid'
+
+    return correl
