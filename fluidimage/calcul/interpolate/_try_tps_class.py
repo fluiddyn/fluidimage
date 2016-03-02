6fe2a6e07da3193224a97a7ab619726d3a3d963f
cornic <guillaume.maurice@legi.grenoble-inp.fr>
2016-03-02 11:12:17 +0100

    add two griddata methods in _try_tps_class.py

--- fluidimage/calcul/interpolate/_try_tps_class.py
+++ fluidimage/calcul/interpolate/_try_tps_class.py
@@ -1,5 +1,9 @@
 import numpy as np
 
+import scipy as sc
+
+from matplotlib.mlab import griddata
+from scipy import interpolate
 from numpy import pi
 
 import matplotlib.pyplot as plt
@@ -10,6 +14,8 @@ from fluidimage.calcul.interpolate.thin_plate_spline import (
 n0 = 100
 n1 = 100
 
+method = 'griddata_scipy'  # 'tps'
+
 
 def myplot(i, x, y, U):
     plt.figure(i)
@@ -19,35 +25,51 @@ def myplot(i, x, y, U):
 
 
 # set of random x coordinates from 0 to 2pi
-x = 2*np.pi*np.random.rand(n0)
-y = 2*np.pi*np.random.rand(n0)
+x = 2 * np.pi * np.random.rand(n0)
+y = 2 * np.pi * np.random.rand(n0)
 
-U = np.exp(-((x-pi)**2 + (y-pi)**2))  # gaussian
+U = np.exp(-((x - pi) ** 2 + (y - pi) ** 2))  # gaussian
 # U = x  # linear
 
 myplot(0, x, y, U)
 
-
-# calculate tps coeff
 centers = np.vstack([x, y])
-U_smooth, U_tps = compute_tps_coeff(centers, U, 0)
-
 # interpolation grid
-xI = yI = np.linspace(0, 2*pi, n1)
+xI = yI = np.linspace(0, 2 * pi, n1)
 XI, YI = np.meshgrid(xI, yI)
 XI = XI.ravel()
 YI = YI.ravel()
 
 new_positions = np.vstack([XI, YI])
 
-# evaluate interpolation on the new grid
-tps = ThinPlateSpline(new_positions, centers)
+if method == 'tps':
 
-U_eval = tps.compute_field(U_tps)
+    # calculate tps coeff
+    U_smooth, U_tps = compute_tps_coeff(centers, U, 0)
+    # evaluate interpolation on the new grid
+    tps = ThinPlateSpline(new_positions, centers)
 
+    U_eval = tps.compute_field(U_tps)
+
+# U_eval = griddata(x,y,U,xI,yI,'linear') #griddata method from matplotlib
+
+    DUX_eval, DUY_eval = tps.compute_gradient(U_tps)
+
+elif method == 'griddata_mpl':  #
+
+    U_eval = griddata(x, y, U, xI, yI, 'linear')
+
+    DUX_eval, DUY_eval = sc.gradient(U_eval)
+
+elif method == 'griddata_scipy':
+
+    grid_x, grid_y = np.meshgrid(xI, yI)
+    U_eval = interpolate.griddata(centers.T, U, (grid_x, grid_y), 'cubic', 0) # nearest, linear, cubic
+
+    DUX_eval, DUY_eval = sc.gradient(U_eval
+                                     )
 myplot(1, XI, YI, U_eval)
 
-DUX_eval, DUY_eval = tps.compute_gradient(U_tps)
 
 myplot(2, XI, YI, DUX_eval)
 myplot(3, XI, YI, DUY_eval)
