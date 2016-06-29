d758d9fbe9621a697c96851c12f7320d8820c87f
paugier <pierre.augier@ens-lyon.org>
2016-06-29 11:37:01 +0200

    Clean up.

--- fluidimage/calibration/apply_calib.py
+++ fluidimage/calibration/apply_calib.py
@@ -6,44 +6,48 @@ Created on Fri Apr 22 15:56:57 2016
 """
 import numpy as np
 
+
 def pix2phys_UV(calib, X, Y, dx, dy, Zindex):
     """
         apply Tsai Calibration to the field
-        
-        Be careful: 
+
+        Be careful:
         The displacement is 3 component BUT
-        it corresponds to the real 3 component displacement projected on the 
+        it corresponds to the real 3 component displacement projected on the
         corresponding plane indexed by Zindex.
     """
     Xphys, Yphys, Zphys = pix2phys(calib, X, Y, Zindex)
-    
+
     dxb, dyb, dzb = pix2phys(calib, X + dx/2.0, Y + dy/2.0, Zindex)
     dxa, dya, dza = pix2phys(calib, X - dx/2.0, Y - dy/2.0, Zindex)
     dxphys = dxb - dxa
     dyphys = dyb - dya
-    dzphys = dzb - dza    
+    dzphys = dzb - dza
     return Xphys, Yphys, Zphys, dxphys, dyphys, dzphys
 
+
 def pix2phys(calib, X, Y, Zindex):
-    
+
     # determine position of Z0
     testangle = 0
-    if hasattr(calib.slices,'sliceAngle') and np.any(calib.slices.sliceAngle[Zindex] != np.asarray([0,0,0])):
-        testangle = 1         
-        om = np.linalg.norm(calib.slices.sliceAngle[Zindex]) 
-        axis_rot = calib.slices.sliceAngle[Zindex]/ om      
+    if hasattr(calib.slices, 'sliceAngle') and \
+       np.any(calib.slices.sliceAngle[Zindex] != np.asarray([0, 0, 0])):
+        testangle = 1
+        om = np.linalg.norm(calib.slices.sliceAngle[Zindex])
+        axis_rot = calib.slices.sliceAngle[Zindex] / om
         cos_om = np.cos(np.pi*om/180.0)
         sin_om = np.sin(np.pi*om/180.0)
-        coeff = axis_rot[2]*(1-cos_om);
+        coeff = axis_rot[2]*(1-cos_om)
         norm_plane = np.zeros(3)
-        norm_plane[0] = axis_rot[0]*coeff+axis_rot[1]*sin_om;
-        norm_plane[1] = axis_rot[1]*coeff-axis_rot[0]*sin_om;
-        norm_plane[2] = axis_rot[2]*coeff+cos_om;
-        Z0 = np.dot(norm_plane,(calib.slices.zsliceCoord[Zindex]))/norm_plane[2];
-    else: 
+        norm_plane[0] = axis_rot[0]*coeff+axis_rot[1]*sin_om
+        norm_plane[1] = axis_rot[1]*coeff-axis_rot[0]*sin_om
+        norm_plane[2] = axis_rot[2]*coeff+cos_om
+        Z0 = np.dot(norm_plane,
+                    (calib.slices.zsliceCoord[Zindex]))/norm_plane[2]
+    else:
         Z0 = calib.slices.zsliceCoord[Zindex][2]
     Z0virt = Z0
-    
+
     if hasattr(calib, 'f') is False:
         calib.f = np.asarray([1, 1])
     if hasattr(calib, 'T') is False:
@@ -55,7 +59,7 @@ def pix2phys(calib, X, Y, Zindex):
 
     if hasattr(calib, 'R'):
         R = calib.R
-        
+
         if testangle:
             a = -norm_plane[0]/norm_plane[2]
             b = -norm_plane[1]/norm_plane[2]
@@ -78,46 +82,46 @@ def pix2phys(calib, X, Y, Zindex):
         Z22 = R[2]*R[6]-R[0]*R[8]
         Zx0 = R[2]*R[4]-R[1]*R[5]
         Zy0 = R[0]*R[5]-R[2]*R[3]
-        
+
         A11 = R[7]*Ty-R[4]*Tz+Z11*Z0virt
         A12 = R[1]*Tz-R[7]*Tx+Z12*Z0virt
         A21 = -R[6]*Ty+R[3]*Tz+Z21*Z0virt
         A22 = -R[0]*Tz+R[6]*Tx+Z22*Z0virt
-        
-        #%     X0=Calib.fx_fy(1)*(R(5)*Tx-R(2)*Ty+Zx0*Z0virt);
-        #%     Y0=Calib.fx_fy(2)*(-R(4)*Tx+R(1)*Ty+Zy0*Z0virt);
-        X0 = (R[4]*Tx-R[1]*Ty+Zx0*Z0virt);
-        Y0 = (-R[3]*Tx+R[0]*Ty+Zy0*Z0virt);
-        #%px to camera:
-        #%     Xd=dpx*(X-Calib.Cx_Cy(1)); % sensor coordinates
-        #%     Yd=(Y-Calib.Cx_Cy(2));
-        Xd = (X-calib.C[0])/calib.f[0] #% sensor coordinates
+
+        #     X0=Calib.fx_fy(1)*(R(5)*Tx-R(2)*Ty+Zx0*Z0virt)
+        #     Y0=Calib.fx_fy(2)*(-R(4)*Tx+R(1)*Ty+Zy0*Z0virt)
+        X0 = (R[4]*Tx-R[1]*Ty+Zx0*Z0virt)
+        Y0 = (-R[3]*Tx+R[0]*Ty+Zy0*Z0virt)
+        # px to camera:
+        #     Xd=dpx*(X-Calib.Cx_Cy(1)) % sensor coordinates
+        #     Yd=(Y-Calib.Cx_Cy(2))
+        Xd = (X-calib.C[0])/calib.f[0]  # sensor coordinates
         Yd = (Y-calib.C[1])/calib.f[1]
-        dist_fact = 1 + calib.kc*(Xd*Xd+Yd*Yd) #%/(f*f); %distortion factor
-        Xu = Xd/dist_fact #%undistorted sensor coordinates
+        dist_fact = 1 + calib.kc*(Xd*Xd+Yd*Yd)  # /(f*f) %distortion factor
+        Xu = Xd/dist_fact  # undistorted sensor coordinates
         Yu = Yd/dist_fact
         denom = Dx*Xu+Dy*Yu+D0
-        Xphys=(A11*Xu+A12*Yu+X0)/denom #%world coordinates
-        Yphys=(A21*Xu+A22*Yu+Y0)/denom
+        Xphys = (A11*Xu+A12*Yu+X0)/denom  # world coordinates
+        Yphys = (A21*Xu+A22*Yu+Y0)/denom
         if testangle:
             Zphys = Z0+a*Xphys+b*Yphys
         else:
-            Zphys = Z0;
+            Zphys = Z0
     else:
-        Xphys=-calib.T[0]+X/calib.f[0]
-        Yphys=-calib.T[1]+Y/calib.f[1]
-        Zphys=X*0
-    Xphys /= 100 # cm to m
-    Yphys /= 100 # cm to m
-    Zphys /= 100 # cm to m
+        Xphys = -calib.T[0]+X/calib.f[0]
+        Yphys = -calib.T[1]+Y/calib.f[1]
+        Zphys = X*0
+    Xphys /= 100  # cm to m
+    Yphys /= 100  # cm to m
+    Zphys /= 100  # cm to m
 
     return Xphys, Yphys, Zphys
-    
-    
-def phys2pix(calib,Xphys,Yphys,Zphys=0):
-    Xphys *= 100 # m to cm
-    Yphys *= 100 # m to cm
-    Zphys *= 100 # m to cm
+
+
+def phys2pix(calib, Xphys, Yphys, Zphys=0):
+    Xphys *= 100  # m to cm
+    Yphys *= 100  # m to cm
+    Zphys *= 100  # m to cm
 
     if hasattr(calib, 'f') is False:
         calib.f = np.asarray([1, 1])
@@ -127,34 +131,32 @@ def phys2pix(calib,Xphys,Yphys,Zphys=0):
     # general case
     if hasattr(calib, 'R'):
         R = calib.R
-        
-        #%camera coordinates
-        xc=R[0]*Xphys+R[1]*Yphys+R[2]*Zphys+calib.T[0]
-        yc=R[3]*Xphys+R[4]*Yphys+R[5]*Zphys+calib.T[1]
-        zc=R[6]*Xphys+R[7]*Yphys+R[8]*Zphys+calib.T[2]
-    
-        #%undistorted image coordinates
-        Xu=xc/zc;
-        Yu=yc/zc;
-    
-        #%radial quadratic correction factor
+
+        # camera coordinates
+        xc = R[0]*Xphys+R[1]*Yphys+R[2]*Zphys+calib.T[0]
+        yc = R[3]*Xphys+R[4]*Yphys+R[5]*Zphys+calib.T[1]
+        zc = R[6]*Xphys+R[7]*Yphys+R[8]*Zphys+calib.T[2]
+
+        # undistorted image coordinates
+        Xu = xc/zc
+        Yu = yc/zc
+
+        # radial quadratic correction factor
         if hasattr(calib, 'kc') is False:
-            r2=1; #no quadratic distortion
+            r2 = 1  # no quadratic distortion
         else:
-            r2=1 + calib.kc*(Xu*Xu+Yu*Yu)
-    
-        #%pixel coordinates
+            r2 = 1 + calib.kc*(Xu*Xu+Yu*Yu)
+
+        # pixel coordinates
         if hasattr(calib, 'C') is False:
-            calib.C=np.asarray([0, 0]) #%default value
-        
-        X=calib.f[0]*Xu*r2 + calib.C[0]
-        Y=calib.f[1]*Yu*r2 + calib.C[1]
-    
-    #%case 'rescale'    
-    else: 
-        X=calib.f[0]*(Xphys+calib.T[0])
-        Y=calib.f[1]*(Yphys+calib.T[1]) 
-        
+            calib.C = np.asarray([0, 0])  # default value
+
+        X = calib.f[0]*Xu*r2 + calib.C[0]
+        Y = calib.f[1]*Yu*r2 + calib.C[1]
+
+    # case 'rescale'
+    else:
+        X = calib.f[0]*(Xphys+calib.T[0])
+        Y = calib.f[1]*(Yphys+calib.T[1])
+
     return X, Y
- 
-    
--- fluidimage/calibration/get_calib.py
+++ fluidimage/calibration/get_calib.py
@@ -10,57 +10,65 @@ import xml.sax.handler
 import numpy as np
 from fluiddyn.util.paramcontainer import ParamContainer, tidy_container
 
+
 class DataObject(object):
     pass
+
 
 def get_number_from_string(string):
     return map(float, re.findall(r"[-+]?\d*\.\d+|\d+", string))
 
 
 def calib_parameters_from_uvmat(fic):
-    calib = ParamContainer(path_file = fic)
+    calib = ParamContainer(path_file=fic)
     tidy_container(calib)
-    
+
     GeometryCalib = calib['geometry_calib']
-    
+
     calib_param = DataObject()
-    calib_param.f = map(float,np.asarray(get_number_from_string(GeometryCalib['fx_fy'])))
+    calib_param.f = map(
+        float, np.asarray(get_number_from_string(GeometryCalib['fx_fy'])))
     calib_param.C = np.asarray(get_number_from_string(GeometryCalib['cx__cy']))
     calib_param.kc = np.asarray(GeometryCalib['kc'])
-    calib_param.T = np.asarray(get_number_from_string(GeometryCalib['tx__ty__tz']))
-  
-    
-    calib_param.R = [] 
+    calib_param.T = np.asarray(
+        get_number_from_string(GeometryCalib['tx__ty__tz']))
+
+    calib_param.R = []
     for i in range(3):
-        calib_param.R = np.hstack([calib_param.R, get_number_from_string(GeometryCalib['r_{}'.format(i+1)])])
-        
+        calib_param.R = np.hstack([
+            calib_param.R,
+            get_number_from_string(GeometryCalib['r_{}'.format(i+1)])])
+
     calib_param.omc = np.asarray(get_number_from_string(GeometryCalib['omc']))
-    
+
     if GeometryCalib['nb_slice'] is not None:
         slices = DataObject()
 
         slices.nbSlice = np.asarray(GeometryCalib['nb_slice'])
-        slices.zsliceCoord = np.zeros([slices.nbSlice,3])
-        
+        slices.zsliceCoord = np.zeros([slices.nbSlice, 3])
+
         for i in range(slices.nbSlice):
-            slices.zsliceCoord[i][:] = get_number_from_string( GeometryCalib['slice_coord_{}'.format(i+1)])
-        
+            slices.zsliceCoord[i][:] = get_number_from_string(
+                GeometryCalib['slice_coord_{}'.format(i+1)])
+
         if GeometryCalib['slice_angle_1'] is not None:
-            slices.sliceAngle = np.zeros([slices.nbSlice,3])
-            for i in range (slices.nbSlice):
-                slices.sliceAngle[i][:] = get_number_from_string( GeometryCalib['slice_angle_{}'.format(i+1)])
-        
+            slices.sliceAngle = np.zeros([slices.nbSlice, 3])
+            for i in range(slices.nbSlice):
+                slices.sliceAngle[i][:] = get_number_from_string(
+                    GeometryCalib['slice_angle_{}'.format(i+1)])
+
         calib_param.slices = slices
-        
+
     return calib_param
 
-    
+
 def xml2obj(src):
     """
     A simple function to converts XML data into native Python object.
     """
 
     non_id_char = re.compile('[^_0-9a-zA-Z]')
+
     def _name_mangle(name):
         return non_id_char.sub('_', name)
 
@@ -68,23 +76,29 @@ def xml2obj(src):
         def __init__(self):
             self._attrs = {}    # XML attributes and child elements
             self.data = None    # child text data
+
         def __len__(self):
             # treat single element as a list of 1
             return 1
+
         def __getitem__(self, key):
             if isinstance(key, basestring):
-                return self._attrs.get(key,None)
+                return self._attrs.get(key, None)
             else:
                 return [self][key]
+
         def __contains__(self, name):
-            return self._attrs.has_key(name)
+            return name in self._attrs
+
         def __nonzero__(self):
             return bool(self._attrs or self.data)
+
         def __getattr__(self, name):
             if name.startswith('__'):
                 # need to do this for Python special methods???
                 raise AttributeError(name)
-            return self._attrs.get(name,None)
+            return self._attrs.get(name, None)
+
         def _add_xml_attr(self, name, value):
             if name in self._attrs:
                 # multiple attribute of the same name are represented by a list
@@ -95,13 +109,16 @@ def xml2obj(src):
                 children.append(value)
             else:
                 self._attrs[name] = value
+
         def __str__(self):
             return self.data or ''
+
         def __repr__(self):
             items = sorted(self._attrs.items())
             if self.data:
                 items.append(('data', self.data))
-            return u'{%s}' % ', '.join([u'%s:%s' % (k,repr(v)) for k,v in items])
+            return u'{%s}' % ', '.join([u'%s:%s' % (
+                k, repr(v)) for k, v in items])
 
     class TreeBuilder(xml.sax.handler.ContentHandler):
         def __init__(self):
@@ -109,6 +126,7 @@ def xml2obj(src):
             self.root = DataNode()
             self.current = self.root
             self.text_parts = []
+
         def startElement(self, name, attrs):
             self.stack.append((self.current, self.text_parts))
             self.current = DataNode()
@@ -116,6 +134,7 @@ def xml2obj(src):
             # xml attributes --> python attributes
             for k, v in attrs.items():
                 self.current._add_xml_attr(_name_mangle(k), v)
+
         def endElement(self, name):
             text = ''.join(self.text_parts).strip()
             if text:
@@ -127,11 +146,12 @@ def xml2obj(src):
                 obj = text or ''
             self.current, self.text_parts = self.stack.pop()
             self.current._add_xml_attr(_name_mangle(name), obj)
+
         def characters(self, content):
             self.text_parts.append(content)
 
     builder = TreeBuilder()
-    if isinstance(src,basestring):
+    if isinstance(src, basestring):
         xml.sax.parseString(src, builder)
     else:
         xml.sax.parse(src, builder)
