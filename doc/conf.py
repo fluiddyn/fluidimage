a5f7089c8a468198cbf5b1f847350660f9887e24
paugier <pierre.augier@ens-lyon.org>
2017-04-11 10:21:09 +0200

    Use conda-forge (bug in readthedocs).

--- doc/conf.py
+++ doc/conf.py
@@ -51,7 +51,6 @@ extensions = [
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode', 'sphinx.ext.autosummary',
     'numpydoc',
-    'IPython.sphinxext.ipython_console_highlighting'
     # 'mathmacro',
 ]
 
