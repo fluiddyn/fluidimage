e02d9626f31f6bfc8c747c4999130fbb2ec73818
paugier <pierre.augier@ens-lyon.org>
2018-07-04 18:45:48 +0200

    trio+multiprocessing+mpi4py: spawn processes and communicate

---
+++ try/trio_spawn_comm_mpi.py
@@ -0,0 +1,64 @@
+
+import sys
+from functools import partial
+
+import trio
+
+import numpy as np
+from mpi4py import MPI
+
+async def sleep():
+    print("enter sleep")
+    await trio.sleep(0.2)
+    print("end sleep")
+
+def cpu_bounded_task(input_data):
+    print("cpu_bounded_task starting")
+    result = input_data.copy()
+    for i in range(1000000-1):
+        result += input_data
+    print("cpu_bounded_task finished ")
+    return result
+
+if "server" not in sys.argv:
+    comm = MPI.COMM_WORLD.Spawn(sys.executable,
+                                args=['trio_spawn_comm_mpi.py', 'server'])
+
+    async def client():
+        input_data = np.arange(4)
+        print("in client: sending the input_data", input_data)
+        send = partial(comm.send, dest=0, tag=0)
+        await trio.run_sync_in_worker_thread(send, input_data)
+
+        print("in client: recv")
+        recv = partial(comm.recv, tag=1)
+        result = await trio.run_sync_in_worker_thread(recv)
+        print("in client: result received", result)
+
+    async def parent():
+        async with trio.open_nursery() as nursery:
+            nursery.start_soon(sleep)
+            nursery.start_soon(client)
+            nursery.start_soon(sleep)
+
+    trio.run(parent)
+
+    print("in client, end")
+    comm.barrier()
+
+else:
+    comm = MPI.Comm.Get_parent()
+
+    async def main_server():
+        # get the data to be processed
+        recv = partial(comm.recv, tag=0)
+        input_data = await trio.run_sync_in_worker_thread(recv)
+        print("in server: input_data received", input_data)
+        # a CPU-bounded task
+        result = cpu_bounded_task(input_data)
+        print("in server: sending back the answer", result)
+        send = partial(comm.send, dest=0, tag=1)
+        await trio.run_sync_in_worker_thread(send, result)
+
+    trio.run(main_server)
+    comm.barrier()
---
+++ try/trio_spawn_comm_multiproc.py
@@ -0,0 +1,60 @@
+
+from multiprocessing import Process, Queue
+
+import trio
+
+import numpy as np
+
+
+async def sleep():
+    print("enter sleep1s")
+    await trio.sleep(0.2)
+    print("end sleep1s")
+
+
+def cpu_bounded_task(input_data):
+    result = input_data.copy()
+    for i in range(1000000-1):
+        result += input_data
+    return result
+
+
+def server(q_c2s, q_s2c):
+    async def main_server():
+        # get the data to be processed
+        input_data = await trio.run_sync_in_worker_thread(q_c2s.get)
+        print("in server: input_data received", input_data)
+        # a CPU-bounded task
+        result = cpu_bounded_task(input_data)
+        print("in server: sending back the answer", result)
+        await trio.run_sync_in_worker_thread(q_s2c.put, result)
+
+    trio.run(main_server)
+
+
+async def client(q_c2s, q_s2c):
+    input_data = np.arange(10)
+    print("in client: sending the input_data", input_data)
+    await trio.run_sync_in_worker_thread(q_c2s.put, input_data)
+    result = await trio.run_sync_in_worker_thread(q_s2c.get)
+    print("in client: result received", result)
+
+
+async def parent(q_c2s, q_s2c):
+
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(sleep)
+        nursery.start_soon(client, q_c2s, q_s2c)
+        nursery.start_soon(sleep)
+
+
+def main():
+    q_c2s = Queue()
+    q_s2c = Queue()
+    p = Process(target=server, args=(q_c2s, q_s2c))
+    p.start()
+    trio.run(parent, q_c2s, q_s2c)
+    p.join()
+
+if __name__ == '__main__':
+    main()
