b8d20b8ab85432bed284e8fe8c98b58c61fb380f
paugier <pierre.augier@ens-lyon.org>
2018-07-11 16:12:05 +0200

    trio, cpu-bounded tasks and the GIL!

---
+++ try/cpubounded.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+
+# pythran export long_func(int)
+
+def long_func(n):
+    a = np.arange(n)
+    return a**3
+
+
+# pythran export long_func1(int)
+
+def long_func1(n):
+    nloops = 10000
+    a = np.arange(n//nloops)
+    result = a
+    for i in range(nloops):
+        result += a**3 + a**2 + 2
