4739d8a16ac07b80e53d3016da194bb43d5b74ee
Ashwin Vishnu <avmo@kth.se>
2018-09-04 02:52:10 +0200

    Preproc coverage

--- fluidimage/preproc/test_base.py
+++ fluidimage/preproc/test_base.py
@@ -7,14 +7,15 @@ from shutil import rmtree
 
 from fluiddyn.io import stdout_redirected
 
-from fluidimage.topologies.preproc import TopologyPreproc
+from fluidimage.preproc.base import PreprocBase
 from fluiddyn.io.image import imread, imsave
 
 from fluidimage import path_image_samples
 
 
-class TestPreprocTemporal(unittest.TestCase):
-    name = "Jet"
+class TestPreprocKarman(unittest.TestCase):
+
+    name = "Karman"
 
     @classmethod
     def setUpClass(cls):
@@ -39,24 +40,38 @@ class TestPreprocTemporal(unittest.TestCase):
         rmtree(os.path.split(cls._work_dir)[0], ignore_errors=True)
 
     def test_preproc(self):
-        """Test preproc subpackage on image sample Jet with two indices."""
-        params = TopologyPreproc.create_default_params()
+        """Test preproc subpackage on image sample Karman with one index."""
+        params = PreprocBase.create_default_params()
 
         params.preproc.series.path = self._work_dir
-        params.preproc.series.strcouple = "i:i+2,1"
-        params.preproc.series.ind_start = 60
 
         for tool in params.preproc.tools.available_tools:
-            if "temporal" in tool:
+            if "sliding" not in tool and "temporal" not in tool:
                 tool = params.preproc.tools.__getitem__(tool)
                 tool.enable = True
 
-        params.preproc.saving.how = "recompute"
-        params.preproc.saving.postfix = "preproc_test"
+        preproc = PreprocBase(params)
+        preproc()
+        preproc.display(1, hist=True)
 
-        with stdout_redirected():
-            topology = TopologyPreproc(params, logging_level="debug")
-            topology.compute()
+
+class TestPreprocTime(TestPreprocKarman):
+    name = "Jet"
+
+    def test_preproc(self):
+        """Test preproc subpackage on image sample Jet with two indices."""
+        params = PreprocBase.create_default_params()
+
+        params.preproc.series.path = self._work_dir
+
+        for tool in params.preproc.tools.available_tools:
+            if "sliding" in tool:
+                tool = params.preproc.tools.__getitem__(tool)
+                tool.enable = True
+
+        preproc = PreprocBase(params)
+        preproc()
+        preproc.display(1, hist=False)
 
 
 if __name__ == "__main__":
