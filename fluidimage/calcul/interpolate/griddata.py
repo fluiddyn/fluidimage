53bb42141de99cc100dc157c99de65591e48fe25
Clement Savaro <clement.savaro@wanadoo.fr>
2020-04-14 11:22:27 +0200

    fix issue 20

--- fluidimage/calcul/interpolate/griddata.py
+++ fluidimage/calcul/interpolate/griddata.py
@@ -9,19 +9,19 @@ from scipy import interpolate
 
 
 def griddata(centers, values, new_positions, using="scipy"):
-    xnew = new_positions[1]
-    ynew = new_positions[0]
+    xnew = new_positions[0]
+    ynew = new_positions[1]
     grid_x, grid_y = np.meshgrid(xnew, ynew)
 
     if using == "scipy":
         # 'linear' or 'cubic'
         values_new = interpolate.griddata(
-            centers.T, values, (grid_y, grid_x), "cubic"
+            centers.T, values, (grid_x, grid_y), "cubic"
         )
     elif using == "matplotlib":
-        x = centers[1]
-        y = centers[0]
-        values_new = mlab.griddata(y, x, values, ynew, xnew, "linear")
+        x = centers[0]
+        y = centers[1]
+        values_new = mlab.griddata(x, y, values, xnew, ynew, "linear")
         values_new[values_new.mask] = np.nan
     else:
         raise ValueError
