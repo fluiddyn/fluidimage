16ed9938e9fe12a16656ad86a53bda1fb4f2a36f
paugier <pierre.augier@univ-grenoble-alpes.fr>
2020-09-18 16:36:08 +0200

    Try to reenable test_launcher on Gitlab CI

--- fluidimage/gui/launcher/test_launcher.py
+++ fluidimage/gui/launcher/test_launcher.py
@@ -1,19 +1,6 @@
-import os
+from .main import Program
 
-from warnings import warn
 
-GITLAB_CI = os.getenv("GITLAB_CI")
-
-if not GITLAB_CI:
-
-    from .main import Program
-
-    def test_launcher(qtbot):
-        widget = Program()
-        qtbot.addWidget(widget)
-
-else:
-    warn(
-        "Skip test in fluidimage.gui.launcher.test_launcher "
-        f"because GITLAB_CI={GITLAB_CI}"
-    )
+def test_launcher(qtbot):
+    widget = Program()
+    qtbot.addWidget(widget)
