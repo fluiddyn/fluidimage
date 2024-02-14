ca4dcd68b9f86610ed949f84b356b41ec97ef6f6
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-02-14 17:34:15 +0100

    Transonic-Pythran

--- fluidimage/topologies/example.py
+++ fluidimage/topologies/example.py
@@ -27,10 +27,10 @@ A = "uint8[:,:]"
 def cpu1(array1: A, array2: A, nloops: int = 10):
     a = np.arange(10000000 // nloops)
     result = a
-    for i in range(nloops):
+    for _ in range(nloops):
         result += a**3 + a**2 + 2
 
-    for i in range(nloops):
+    for _ in range(nloops):
         array1 = array1 * array2
     return (array1, array1)
 
@@ -39,10 +39,10 @@ def cpu1(array1: A, array2: A, nloops: int = 10):
 def cpu2(array1: A, array2: A, nloops: int = 10):
     a = np.arange(10000000 // nloops)
     result = a
-    for i in range(nloops):
+    for _ in range(nloops):
         result += a**3 + a**2 + 2
 
-    for i in range(nloops):
+    for _ in range(nloops):
         array1 = np.multiply(array1, array2)
     return array1
 
