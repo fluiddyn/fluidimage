3d277dc3f678cbe7e20f8f8c62afa410b9af6b7d
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-03-17 23:35:36 +0100

    Replace interp2d by LinearNDInterpolator

--- src/fluidimage/calcul/smooth_clean.py
+++ src/fluidimage/calcul/smooth_clean.py
@@ -1,11 +1,13 @@
-"""Smooth 2d fields (:mod:`fluidimage.calcul.smooth_clean`)
-===========================================================
+"""Smooth 2d fields
+===================
 
 
 """
 
+import itertools
+
 import numpy as np
-from scipy.interpolate import interp2d
+from scipy.interpolate import LinearNDInterpolator
 from scipy.ndimage import convolve
 
 from fluidimage.calcul.interpolate.griddata import griddata
@@ -28,40 +30,41 @@ def smooth_clean(xs, ys, deltaxs, deltays, iyvecs, ixvecs, threshold):
     for_norm = np.ones(shape)
 
     selection = ~(np.isnan(deltaxs) | np.isnan(deltays))
-    if selection.any():
-        centers = np.vstack([xs[selection], ys[selection]])
-        dxs_select = deltaxs[selection]
-        dys_select = deltays[selection]
-        dxs = griddata(centers, dxs_select, (ixvecs, iyvecs)).reshape([ny, nx])
-        dys = griddata(centers, dys_select, (ixvecs, iyvecs)).reshape([ny, nx])
+    if not selection.any():
+        return deltaxs, deltays
 
-        dxs2 = _smooth(dxs, for_norm)
-        dys2 = _smooth(dys, for_norm)
+    centers = np.vstack([xs[selection], ys[selection]])
+    dxs_select = deltaxs[selection]
+    dys_select = deltays[selection]
+    dxs = griddata(centers, dxs_select, (ixvecs, iyvecs)).reshape([ny, nx])
+    dys = griddata(centers, dys_select, (ixvecs, iyvecs)).reshape([ny, nx])
 
-        inds = (abs(dxs2 - dxs) + abs(dys2 - dys) > threshold).nonzero()
+    dxs2 = _smooth(dxs, for_norm)
+    dys2 = _smooth(dys, for_norm)
 
-        dxs[inds] = 0
-        dys[inds] = 0
-        for_norm[inds] = 0
+    inds = (abs(dxs2 - dxs) + abs(dys2 - dys) > threshold).nonzero()
 
-        dxs_smooth = _smooth(dxs, for_norm)
-        dys_smooth = _smooth(dys, for_norm)
+    dxs[inds] = 0
+    dys[inds] = 0
+    for_norm[inds] = 0
 
-        # come back to the unstructured grid
-        fxs = interp2d(ixvecs, iyvecs, dxs_smooth, kind="linear")
-        fys = interp2d(ixvecs, iyvecs, dys_smooth, kind="linear")
+    dxs_smooth = _smooth(dxs, for_norm)
+    dys_smooth = _smooth(dys, for_norm)
 
-        out_dxs = np.empty_like(deltaxs)
-        out_dys = np.empty_like(deltays)
+    # come back to the unstructured grid
+    xy = list(itertools.product(ixvecs, iyvecs))
+    interpolator_x = LinearNDInterpolator(xy, dxs_smooth.T.flat)
+    interpolator_y = LinearNDInterpolator(xy, dys_smooth.T.flat)
+    out_dxs = interpolator_x(xs, ys)
+    out_dys = interpolator_y(xs, ys)
 
-        for i, (x, y) in enumerate(zip(xs, ys)):
-            out_dxs[i] = fxs(x, y)[0]
-            out_dys[i] = fys(x, y)[0]
+    # previous implementation (interp2d is depreciated)
+    # fxs = interp2d(ixvecs, iyvecs, dxs_smooth, kind="linear")
+    # fys = interp2d(ixvecs, iyvecs, dys_smooth, kind="linear")
+    # out_dxs = np.empty_like(deltaxs)
+    # out_dys = np.empty_like(deltays)
+    # for i, (x, y) in enumerate(zip(xs, ys)):
+    #     out_dxs[i] = fxs(x, y)[0]
+    #     out_dys[i] = fys(x, y)[0]
 
-    # from fluiddyn import ipydebug
-    # import matplotlib.pylab as plt
-    # plt.ion()
-    # ipydebug()
-    else:
-        out_dxs, out_dys = deltaxs, deltays
     return out_dxs, out_dys
