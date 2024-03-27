aa923e4bdb3efbc054547d17a5772a979a7dccc1
paugier <pierre.augier@univ-grenoble-alpes.fr>
2024-03-27 13:17:35 +0100

    Improve examples

--- doc/examples/piv_parallel.py
+++ doc/examples/piv_parallel.py
@@ -5,11 +5,17 @@ params = Topology.create_default_params()
 
 params.series.path = get_path_image_samples() / "Karman/Images"
 
-params.piv0.shape_crop_im0 = 32
-params.multipass.number = 2
-params.multipass.use_tps = False
+params.mask.strcrop = "50:350, 0:380"
 
-params.mask.strcrop = ":, 50:500"
+params.piv0.shape_crop_im0 = 32
+params.piv0.displacement_max = 5
+params.piv0.nb_peaks_to_search = 2
+
+params.fix.correl_min = 0.4
+params.fix.threshold_diff_neighbour = 2.0
+
+params.multipass.number = 2
+params.multipass.use_tps = "last"
 
 # params.saving.how = 'complete'
 params.saving.postfix = "piv_example"
--- doc/examples/piv_try_params_Karman.py
+++ doc/examples/piv_try_params_Karman.py
@@ -4,24 +4,25 @@ from fluidimage.piv import Work
 
 params = Work.create_default_params()
 
-params.series.path = "../../image_samples/Oseen/Images"
+params.series.path = "../../image_samples/Karman/Images"
 
-params.mask.strcrop = "30:250, 100:"
+params.mask.strcrop = "50:350, 0:380"
+
+params.fix.correl_min = 0.4
+params.fix.threshold_diff_neighbour = 2.0
 
 params.piv0.shape_crop_im0 = 32
 params.piv0.displacement_max = 5
-
-params.fix.correl_min = 0.2
-params.fix.threshold_diff_neighbour = 8
+params.piv0.nb_peaks_to_search = 2
 
 params.multipass.number = 2
-params.multipass.use_tps = True
+params.multipass.use_tps = "last"
 
 work = Work(params=params)
 
 piv = work.process_1_serie()
 
-# piv.display(show_interp=True, scale=0.3, show_error=True)
+# piv.display(show_interp=True, scale=0.3, show_error=False)
 piv.display(show_interp=False, scale=1, show_error=True)
 
 # piv.save()
